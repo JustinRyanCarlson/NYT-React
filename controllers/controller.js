abf81638bf84d0f50767a53cbb94677784c3dd62
Justin Carlson <justin.ryan.carlson@gmail.com>
2017-02-28 21:21:58 -0600

    adds required NPM packages, adds webpack config file, adds icons for footer, adds main.handlebars and search_page.handlebars / sets up their layout with Bootstrap, removes code from old HW from controller.js

--- controllers/controller.js
+++ controllers/controller.js
@@ -5,37 +5,19 @@ var scrapedArticles = require('./../models/articles.js');
 
 
 router.get('/api/saved', function (req, res) {
-    scrapedArticles.find().sort('-time').exec(function (err, articles) {
-        if (err) {
-            console.log(err);
-        } else {
-            res.render('scrape.handlebars', { articles: articles });
-        }
-    });
+
 });
 
 router.post('/api/saved', function (req, res) {
-    scrapedArticles.find().sort('-time').exec(function (err, articles) {
-        if (err) {
-            console.log(err);
-        } else {
-            res.render('scrape.handlebars', { articles: articles });
-        }
-    });
+
 });
 
 router.delete('/api/saved', function (req, res) {
-    scrapedArticles.find().sort('-time').exec(function (err, articles) {
-        if (err) {
-            console.log(err);
-        } else {
-            res.render('scrape.handlebars', { articles: articles });
-        }
-    });
+
 });
 
 router.get('*', function (req, res) {
-    res.render('scrape.handlebars', { articles: articles });
+    res.render('search_page.handlebars');
 });
 
 // Export routes for server.js to use.
