4446dfa13c70c3b3dada3c3997775ce9bc278c71
Justin Carlson <justin.ryan.carlson@gmail.com>
2017-03-01 16:38:53 -0600

    adds jquery CDN, adds search_page.js for API call to New York Times, changes the date format of the search form to match what NYTs API wants

--- controllers/controller.js
+++ controllers/controller.js
@@ -1,19 +1,41 @@
 var express = require("express");
 var router = express.Router();
-var scrapedArticles = require('./../models/articles.js');
+var articles = require('./../models/articles.js');
 
 
 
 router.get('/api/saved', function (req, res) {
-
+    articles.find({}, function (err, articles) {
+        if (err) {
+            console.log(err);
+        } else {
+            res.json(articles);
+        }
+    });
 });
 
 router.post('/api/saved', function (req, res) {
-
+    var newArticle = req.body;
+    // need to see what data ill be getting from the front end
+    newArticle.save(function (err, article) {
+        if (err) {
+            console.log(err);
+        } else {
+            res.json('pass');
+        }
+    });
 });
 
 router.delete('/api/saved', function (req, res) {
+    var articleId = req.body;
 
+    articleId.findByIdAndRemove(articleId, function (err, article) {
+        if (err) {
+            console.log(err);
+        } else {
+            res.json('pass');
+        }
+    });
 });
 
 router.get('*', function (req, res) {
---
+++ public/assets/javascript/search_page.js
@@ -0,0 +1,23 @@
+$(document.body).on('click', '#submit', function () {
+    var queryString = "https://api.nytimes.com/svc/search/v2/articlesearch.json";
+    var apiKey = "?api-key=743707e5f8f44f1ca8c6aab3a1a11f39"
+    var topic = '&q=' + $('#topic').val().trim();
+
+    var startManipulate = $('#start-year').val().split('-');
+    startManipulate = startManipulate.join('');
+    var startYear = '&begin_date=' + startManipulate;
+
+    var endManipulate = $('#end-year').val().split('-');
+    endManipulate = endManipulate.join('');
+    var endYear = '&end_date=' + endManipulate;
+
+    var query = queryString + apiKey + topic + startYear + endYear;
+
+    console.log(query);
+
+    $.get(query, function (articles) {
+        console.log(articles);
+    });
+
+    return false;
+});
